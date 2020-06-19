c41a837323c297d4d1da4e1965b32d6754dc81b3
littlestarGit <2743470546@qq.com>
2020-06-19 20:06:01 +0800

    属性，事件，方法 1.设置属性 2.鼠标点击事件 3.鼠标移动事件（显示鼠标的坐标）

---
+++ The1class/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace The1class
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Text = "这是一个应用程序";
+            this.BackColor = Color.FromArgb(255, 255, 0);
+            this.label1.SetBounds(100, 100, 200, 50);
+
+            this.label1.Left += 200;
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.label1.Text = e.X + "," + e.Y;
+        }
+    }
+}
